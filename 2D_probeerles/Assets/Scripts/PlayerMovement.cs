fbc8821bc654bfa3314e39f9f15cb73728ef84f4
MooshiPlant <54902326+MooshiPlant@users.noreply.github.com>
2020-06-21 16:59:23 +0200

    First Commit

---
+++ 2D_probeerles/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+   
+    void Update()
+    {
+
+       Debug.Log(horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            jump = true;
+            animator.SetBool("Jumping", true);
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jumping", false);
+    }
+
+
+    void FixedUpdate()
+    {
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
